0c943344a9047013fd6b9190eb5f3542a731074d
StevenTheBearded <>
2021-04-03 21:31:53 -0400

    refactor

--- src/main.js
+++ src/main.js
@@ -9,17 +9,17 @@ var saveButton = document.querySelector('.save-cover-button');
 var viewButton = document.querySelector('.view-saved-button');
 var makeButton = document.querySelector('.make-new-button');
 var homeButton = document.querySelector('.home-button');
-var newBookButton = document.querySelector('.create-new-book-button')
+var newBookButton = document.querySelector('.create-new-book-button');
 //formViews
-var savedView = document.querySelector('.saved-view')
-var hideHomePage = document.querySelector('.home-view')
-var formView = document.querySelector('.form-view')
+var savedView = document.querySelector('.saved-view');
+var hideHomePage = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
 //input Query Selectors
 var coverInput = document.querySelector('.user-cover');
 var titleInput = document.querySelector('.user-title');
 var descriptorInput1 = document.querySelector('.user-desc1');
 var descriptorInput2 = document.querySelector('.user-desc2');
-var savedCoversGrid = document.querySelector('.saved-covers-section')
+var savedCoversGrid = document.querySelector('.saved-covers-section');
 
 // We've provided a few variables below
 var savedCovers = [];
@@ -28,11 +28,19 @@ var currentCover;
 //Event listeners
 window.addEventListener('load', randomCover);
 randomButton.addEventListener('click', randomCover);
-makeButton.addEventListener('click', viewForm)
-viewButton.addEventListener('click', viewSaveCovers)
-homeButton.addEventListener('click', goHomePage)
-newBookButton.addEventListener('click', createBook)
-saveButton.addEventListener('click', saveMiniPoster)
+
+makeButton.addEventListener('click', function(){
+  navToSection(hideHomePage, formView)
+});
+viewButton.addEventListener('click', function() {
+  navToSection(hideHomePage, savedView)
+});
+homeButton.addEventListener('click', function(){
+  navToSection(savedView, hideHomePage)
+});
+
+saveButton.addEventListener('click', saveMiniPoster);
+newBookButton.addEventListener('click', createBook);
 
 //Event Handlers
 function saveMiniPoster (){
@@ -40,7 +48,7 @@ function saveMiniPoster (){
     savedCovers.push(currentCover);
     showSavedCovers()
   }
-}
+};
 
 function createBook(event){
   event.preventDefault()
@@ -53,46 +61,16 @@ function createBook(event){
     coverTitle.innerText = titles[titles.length-1];
     tagLine1.innerText = descriptors[descriptors.length-2];
     tagLine2.innerText = descriptors[descriptors.length-1];
-}
-
-function homePage(){
-
-}
-
-function goHomePage() {
-  savedView.classList.add('hidden');
-  formView.classList.add('hidden');
-  hideHomePage.classList.remove('hidden')
-  randomButton.classList.remove('hidden');
-  saveButton.classList.remove('hidden');
-  homeButton.classList.add('hidden')
-}
-
-function viewSaveCovers() {
-  savedView.classList.remove('hidden')
-  hideHomePage.classList.add('hidden')
-  saveButton.classList.add('hidden')
-  formView.classList.add('hidden')
-  randomButton.classList.add('hidden')
-  homeButton.classList.remove('hidden')
-}
-
-function viewForm() {
-  formView.classList.remove('hidden')
-  homeButton.classList.remove('hidden')
-  hideHomePage.classList.add('hidden')
-  randomButton.classList.add('hidden');
-  saveButton.classList.add('hidden')
-}
+};
 
 function showSavedCovers(){
-      savedCoversGrid.innerHTML += `<div class='mini-cover'>
+      savedCoversGrid.innerHTML += `<div class='mini-cover' ondblclick="remove(this)>
       <img class='mini-cover' src=${currentCover.cover}>
       <h2 class='cover-title first-letter'>${currentCover.title}</h2>
       <h3 class='tagline'>A tale of ${currentCover.tagline1} and ${currentCover.tagline2}</h3>
       </div>
       `
-}
+};
 
 function randomCover() {
   currentCover = createCover(covers, titles, descriptors);
@@ -100,7 +78,7 @@ function randomCover() {
   coverTitle.innerText = currentCover.title;
   tagLine1.innerText = currentCover.tagline1;
   tagLine2.innerText = currentCover.tagline2;
-}
+};
 
 function createCover(covers, titles, descriptors) {
   return new Cover(
@@ -108,17 +86,22 @@ function createCover(covers, titles, descriptors) {
     getRandomElement(titles),
     getRandomElement(descriptors),
     getRandomElement(descriptors));
-}
+};
 
 function getRandomElement(array) {
-  return array[getRandomIndex(array)]
-}
+  return array[getRandomIndex(array)];
+};
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
 
 function navToSection(origin, destination) {
-  origin.classList.toggle("hidden");
-  destination.classList.toggle("hidden");
-}
+  origin.classList.toggle('hidden');
+  destination.classList.toggle('hidden');
+};
+
+function remove(el) {
+  var element = el;
+  element.remove();
+};
